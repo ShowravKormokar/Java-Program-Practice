6e3adf514d22082b39aff63be8ef385bb1ff5757
ShowravKormokar <showravkormokar354@gmail.com>
2023-11-26 00:00:46 +0600

    Learn Quick Sort Algorithm and It's representation

--- Data-Structure/Sorting/QuickSort.java
+++ Data-Structure/Sorting/QuickSort.java
@@ -1,6 +1,36 @@
 import java.util.*;
 
-public class InsertionSort{
+public class QuickSort{
+
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+
+        for(int j=low; j<high; j++){
+            if(arr[j] < pivot){
+                i++;
+                // Swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i; // pivot index
+    }
+
+    public static void quickSort(int arr[], int low, int high){
+        if(low < high){
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex-1);
+            quickSort(arr, pivotIndex+1, high);
+        }
+    }
+
     public static void main(String agrs[]){
         Scanner sc = new Scanner(System.in);
 
@@ -12,5 +42,13 @@ public class InsertionSort{
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+
+        quickSort(arr, 0, n-1);
+
+        // Print
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
     }
 }
